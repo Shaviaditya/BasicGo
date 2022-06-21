fc433d24cee3da098a2e92b42e214b77b1502aec
Shaviaditya <rudrashiv311@gmail.com>
2022-06-22 01:40:46 +0530

    Some more basic implementations

--- go_authExample/config/config.go
+++ go_authExample/config/config.go
@@ -1,14 +1,15 @@
 package config
 
 import (
+	"github.com/cweill/gotests/gotests/process"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 );
 
 func ConfigSetup() *oauth2.Config {
 	conf := &oauth2.Config{
-		ClientID:     "320086037196-95g0ecmdrna8ov222m5oe4jdoqr26483.apps.googleusercontent.com",
-		ClientSecret: "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",
+		ClientID: "",
+		ClientSecret: "",
 		RedirectURL:  "http://localhost:5500/google/callback",
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
